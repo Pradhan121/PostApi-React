a6fa448a0ca6317ae67e30f84f85bb220b1e0be8
pradhan121 <sm.pradhan101.gmail.com>
2024-04-13 23:13:02 +0530

    First commit

---
+++ src/component1/Form.js
@@ -0,0 +1,75 @@
+import React, { useState } from 'react'
+import axios from 'axios';
+
+const Form =() => {
+    const data = {
+       customerType:"",customerName:"", customerMobile:'',
+       customerEmail:"",customerNo:'',Birthday:'',HealthInsurance:''
+         };
+
+         const[inputdata, setInputdata] = useState(data);
+
+         const handleInput = (e) => {
+           setInputdata({...inputdata,[e.target.name]: e.target.value})
+
+         }
+
+         function handleSubmit(e) {
+          e.preventDefault();
+          axios.post('https://hammerhead-app-tzlph.ondigitalocean.app/customers',  inputdata )
+          .then((resp) => {console.log(resp.inputdata) })
+          .catch(err => console.log(err))
+         }
+
+    return(
+      <>
+         <div className='app'  >
+       <form >
+        <div>
+
+        <label>customerType: </label>
+           <input type="number" onChange={handleInput} id="number" name="customerType" min='0' max='50'  
+              autoComplete='off' />
+                 <br></br>
+                 <br></br>
+       <label> customerName:</label>
+        <input type="text"onChange={handleInput} id="username" name='customerName'
+            autoComplete='off' />
+            
+               <br></br>
+               <br></br>
+          <label> customerMobile:</label>
+          <input type="tel" onChange={handleInput} id="mobileNo" name='customerMobile'
+            autoComplete='off' />
+           
+              <br></br>
+              <br></br>
+         <label htmlFor="emailId"> customerEmail</label>
+          <input type="email"  onChange={handleInput} id="emailId" name='customerEmail'
+            autoComplete='off' />
+            
+              <br></br>
+              <br></br>
+       <label> customerNo:</label>
+         <input type="number"  onChange={handleInput} id="counter" name='customerNo' min='0' max='50'
+            autoComplete='off' />
+           
+              <br></br>
+              <br></br>
+      <label> birthday:</label>
+        <input type="date" onChange={handleInput} id="dateOfBirth" name='birthday'
+            autoComplete='off'/>
+            <br></br>
+            <br></br>
+       <label> healthInsurance:</label>
+        <input type="text"  onChange={handleInput} id="number" name='healthInsurance' />
+           <br></br>
+           <br></br>
+       <button  onClick = {handleSubmit}style={{ backgroundColor:'black' ,color:'white', marginBottom:'10px'}} type='submit'>Login</button>
+          </div>
+        </form>
+        </div>
+    </>
+  );
+}
+export default Form;
---
+++ src/component1/MaterialUiTable.js
@@ -0,0 +1,85 @@
+
+import { Paper,TableContainer,Table,TableHead,TableRow,TableCell,TableBody,TablePagination } from "@mui/material";
+import React, {useEffect, useState } from "react";
+
+function MaterialUiTable() {
+  const columns=[
+     {id:'customerid',name:'customerId'},
+     {id:'customername',name:'customerName'},
+     {id:'customeremail',name:'customerEmail'},
+     {id:'customermobile',name:'customerMobile'},
+     {id:'customerpassword',name:'customerPassword'}
+   ]
+   const[rows,setRow] = useState([]);
+   const[page,setPage] = useState(0);
+   const[rowPage,setRowPage] =  React.useState(10);
+  
+    const handleChangePage=(event, newPage) => {
+      setPage(newPage);
+    };
+    const handleChangeRowsPerPage=(event) => {
+       setRowPage(parseInt(event.target.value,10));
+       setPage(0);
+    };
+
+     useEffect(() => {
+        fetch(`https://hammerhead-app-tzlph.ondigitalocean.app/customers?pageNo=${page}&rowPage=${rowPage}`)
+         .then(resp => {
+           return resp.json();
+       }).then(resp => {
+          setRow(resp.content);
+       }).catch(e => {
+          console.log(e.message);
+       })
+     },[page, rowPage]);
+
+ return(
+   <div>
+    <Paper sx={{ width: '90%',marginLeft:'5%',marginTop:'4%'}}>
+      <TableContainer>
+        <Table stickyHeader>
+         <TableHead>
+           <TableRow>
+           {columns.map((column)=>(
+              <TableCell style={{backgroundColor:'black',color:"white"}} key={column.id}>{column.name}</TableCell>
+           
+          ))}
+           </TableRow>
+         </TableHead>
+           <TableBody>
+             {rows && rows
+             //.slice(page * rowPage, page * rowPage* rowPage  )
+             
+             .map((row,i) => {
+                return(
+
+                <TableRow key={i}>
+                  <TableCell>{row.customerId}</TableCell>
+                  <TableCell>{row.customerName}</TableCell>
+                  <TableCell>{row.customerEmail}</TableCell>
+                  <TableCell>{row.customerMobile}</TableCell>
+                  <TableCell>{row.customerPassword}</TableCell>
+                 </TableRow>
+                )
+             })}
+           </TableBody>
+        </Table>
+      </TableContainer>
+       <TablePagination rowPageOption={[5,10,25]}
+           Component="div"
+            page={page}
+            rowpage={rowPage}
+            count={rows.length}
+            onPageChange={handleChangePage}
+            onRowsPerPageChange= {handleChangeRowsPerPage} 
+             labelRowsPerPage={<span>Rows:</span>}
+         labelDisplayedRows={({ page }) => {
+           return `Page: ${page}`;
+         }}
+       
+        />
+    </Paper> 
+ </div>
+ );
+}
+export default MaterialUiTable;
